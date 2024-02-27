db0fef2b7ac955f35c84c43718d4a6cf63ccea45
QulamSamadli <qulam999@gmail.com>
2024-02-28 00:42:24 +0400

    added new classname and item components

--- src/Components/Logo/index.jsx
+++ src/Components/Logo/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="bg-[#db8336] text-center py-5">
-      <h1 className="text-[60px]">🌴FAR AWAY💼</h1>
+      <h1 className="text-[60px]">🌅FAR AWAY💼</h1>
     </div>
   );
 };
--- src/Components/PackingList/index.jsx
+++ src/Components/PackingList/index.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Item from "../Item";
+import axios from "axios";
+
+const url = "https://faraway-db.vercel.app/posts";
 
 const PackingList = () => {
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    axios.get(url).then(({ data }) => {
+      setPost(data);
+    });
+  }, []);
+
   return (
-    <div className="bg-[#592414] h-[65vh] text-center text-[25px] font-bold text-white py-5 font-mono">
-      LIST
-    </div>
+    <ul className="bg-[#592414] h-[64vh] grid grid-flow-col md:grid-cols-7 gap-4 text-center text-[25px] font-bold text-white py-5 font-mono">
+      {post.map(({ id, description }) => {
+        return <Item key={id} item={description} />;
+      })}
+    </ul>
   );
 };
 
